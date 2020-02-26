bd9a16e9c8ebc7a889c717f4f3d38146081f21d4
Arun Chandran <arun.chandran@Arun-CS.local>
2020-02-27 02:08:46 +0530

    simple build for server

--- build-scripts/webpack.client.js
+++ build-scripts/webpack.client.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const env = process.env.NODE_ENV
-module.export = {
+module.exports = {
   entry: path.resolve(__dirname, '..', '/src/client/index.js'),
   output: {
     path: path.resolve(__dirname, '..', 'dist/client')
--- build-scripts/webpack.server.js
+++ build-scripts/webpack.server.js
@@ -1,32 +1,39 @@
+const webpack = require('webpack')
 const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 const env = process.env.NODE_ENV
 
-module.export = {
+module.exports = {
   target: 'node',
+  mode: env,
+  node: {
+    __dirname: false
+  },
+  externals: [nodeExternals()],
   entry: path.resolve(__dirname, '..', 'src/server/index.js'),
   output: {
-    path: path.resolve(__dirname, '..', 'dist/server'),
+    path: path.resolve(__dirname, '..', 'dist/server/'),
     filename: 'index.js'
   },
-  resolve: {
-    extensions: ['.js', '.jsx', '.scss']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx|\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          babelrc: false,
-          presets: [
-            "@babel/preset-env",
-            "preact"
-          ]
-        }
-      }
-    ]
-  },
+  // resolve: {
+  //   extensions: ['.js', '.jsx', '.scss']
+  // },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.jsx|\.js$/,
+  //       loader: 'babel-loader',
+  //       exclude: /node_modules/,
+  //       options: {
+  //         babelrc: false,
+  //         presets: [
+  //           "@babel/preset-env",
+  //           "preact"
+  //         ]
+  //       }
+  //     }
+  //   ]
+  // },
   devtool: (env === 'production') ? '' : 'source-map'
 }
